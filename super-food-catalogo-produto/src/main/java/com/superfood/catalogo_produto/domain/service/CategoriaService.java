f4321ef4e90094c6e5d29b8154ea29cd43c58550
Fabio Kusaba <fabinhokb44@gmail.com>
2025-11-25 21:33:11 -0300

    adicionar service e repository de Categoria

---
+++ super-food-catalogo-produto/src/main/java/com/superfood/catalogo_produto/domain/service/CategoriaService.java
@@ -0,0 +1,32 @@
+package com.superfood.catalogo_produto.domain.service;
+
+import com.superfood.catalogo_produto.domain.model.Categoria;
+import com.superfood.catalogo_produto.domain.repository.CategoriaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    public Categoria salvar(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria buscarPorId(String id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria inexistente"));
+    }
+
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+
+    public void excluir(String id) {
+        var categoriaExistente = buscarPorId(id);
+        categoriaRepository.delete(categoriaExistente);
+    }
+}
