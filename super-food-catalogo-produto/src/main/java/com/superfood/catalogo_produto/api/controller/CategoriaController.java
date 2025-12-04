0909142acef9ec8354035b560f4766ecf858482a
Fabio Kusaba <fabinhokb44@gmail.com>
2025-12-04 20:12:21 -0300

    criar classe para mapeamento de categoria

--- super-food-catalogo-produto/src/main/java/com/superfood/catalogo_produto/api/controller/CategoriaController.java
+++ super-food-catalogo-produto/src/main/java/com/superfood/catalogo_produto/api/controller/CategoriaController.java
@@ -1,33 +1,26 @@
 package com.superfood.catalogo_produto.api.controller;
 
-import java.util.UUID;
-
-import org.modelmapper.ModelMapper;
+import com.superfood.catalogo_produto.api.dtos.CadastrarCategoriaRequest;
+import com.superfood.catalogo_produto.api.dtos.CadastrarCategoriaResponse;
+import com.superfood.catalogo_produto.api.mapper.CategoriaMapper;
+import com.superfood.catalogo_produto.domain.service.CategoriaService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.superfood.catalogo_produto.api.dtos.CadastrarCategoriaRequest;
-import com.superfood.catalogo_produto.api.dtos.CadastrarCategoriaResponse;
-import com.superfood.catalogo_produto.domain.model.Categoria;
-import com.superfood.catalogo_produto.domain.service.CategoriaService;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/categorias")
 @RequiredArgsConstructor
 public class CategoriaController {
     private final CategoriaService categoriaService;
-    private final ModelMapper modelMapper;
+    private final CategoriaMapper categoriaMapper;
 
     @PostMapping
-    public CadastrarCategoriaResponse cadastrar(@RequestBody CadastrarCategoriaRequest categoria) {
-        var novaCategoria = new Categoria();
-        novaCategoria.setId(UUID.randomUUID().toString());
-        modelMapper.map(categoria, novaCategoria);
-        var categoriaSalva = categoriaService.salvar(novaCategoria);
-        return modelMapper.map(categoriaSalva, CadastrarCategoriaResponse.class);
+    public CadastrarCategoriaResponse cadastrar(@RequestBody CadastrarCategoriaRequest request) {
+        var categoria = categoriaMapper.toDomainModel(request);
+        categoriaService.salvar(categoria);
+        return categoriaMapper.toResponse(categoria);
     }
 }
